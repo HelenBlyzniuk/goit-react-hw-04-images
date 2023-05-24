a4b25ed55b0ff606455ac1a101dce7f94f21020d
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-05-24 08:49:35 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,45 +1,81 @@
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  state = {};
-
-  handleClick = e => {
+export function Modal({ handleModalClick, largeImageURL, tags }) {
+  const handleClick = e => {
     if (e.target.nodeName === 'IMG') {
       return;
     }
-    this.props.handleModalClick();
+    handleModalClick();
   };
 
-  componentDidMount() {
+  useEffect(() => {
     window.addEventListener('keydown', e => {
       if (e.code === 'Escape') {
-        this.props.handleModalClick();
+        handleModalClick();
       }
     });
-  }
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.props.handleModalClick);
-  }
-  render() {
-    const { largeImageURL, tags } = this.props;
-    return createPortal(
-      <div className={css.overlay} onClick={this.handleClick}>
-        <div className={css.modal}>
-          <img src={largeImageURL} alt={tags} width="500" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  useEffect(() => {
+    return window.removeEventListener('keydown', handleModalClick);
+  }, []);
+
+  return createPortal(
+    <div className={css.overlay} onClick={handleClick}>
+      <div className={css.modal}>
+        <img src={largeImageURL} alt={tags} width="500" />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
+
 Modal.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
   tags: PropTypes.string.isRequired,
   handleModalClick: PropTypes.func.isRequired,
 };
+
+// export class Modal extends Component {
+//   state = {};
+
+//   handleClick = e => {
+//     if (e.target.nodeName === 'IMG') {
+//       return;
+//     }
+//     this.props.handleModalClick();
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', e => {
+//       if (e.code === 'Escape') {
+//         this.props.handleModalClick();
+//       }
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.props.handleModalClick);
+//   }
+//   render() {
+//     const { largeImageURL, tags } = this.props;
+//     return createPortal(
+//       <div className={css.overlay} onClick={this.handleClick}>
+//         <div className={css.modal}>
+//           <img src={largeImageURL} alt={tags} width="500" />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
+// Modal.propTypes = {
+//   largeImageURL: PropTypes.string.isRequired,
+//   tags: PropTypes.string.isRequired,
+//   handleModalClick: PropTypes.func.isRequired,
+// };
